c6781fd2ca90e6ee9532d0af0ed1610a2a93292f
wjbohnh <wjbohnh@ccfab4c5-ad45-0410-a689-f63b6aebdeca>
2009-01-30 18:18:47 +0000

    Added Boost RNG (specifically, Mersennes twister) example/"test". If this new test adds value beyond the short term and is something that should be maintained permanently, it should be split-out into its own source file. Request review from Laura.
    git-svn-id: svn+ssh://software.sandia.gov/svn/pecos/trunk@147 ccfab4c5-ad45-0410-a689-f63b6aebdeca

--- test/boost_test_dist.cpp
+++ test/boost_test_dist.cpp
@@ -236,11 +236,88 @@ boost_test_dist::~boost_test_dist()
   }
 }
 
+
+// WJB: RNG testing -- eventually split out into its own rng_test.cpp file
+#ifdef HAVE_BOOST
+#include <boost/random/linear_congruential.hpp>
+#include <boost/random/mersenne_twister.hpp>
+#include <boost/random/uniform_int.hpp>
+#include <boost/random/uniform_real.hpp>
+#include <boost/random/variate_generator.hpp>
+#include <boost/generator_iterator.hpp>
+
+// This is a reproducible simulation experiment.  See main().
+void experiment(boost::mt19937& generator)
+{
+  using namespace boost;
+  // Define a uniform random number distribution of integer values between
+  // 1 and 6 inclusive.
+  //typedef variate_generator<mt19937&, uniform_int<> > gen_type;
+  variate_generator<mt19937&, uniform_int<> >
+    die_gen(generator, uniform_int<>(1, 6));
+
+  // If you want to use an STL iterator interface, use iterator_adaptors.hpp.
+  generator_iterator<variate_generator<mt19937&, uniform_int<> > >
+    die(&die_gen);
+  for(int i = 0; i < 10; ++i)
+    PCout << *die++ << " ";
+  PCout << '\n';
+}
+#endif // HAVE_BOOST
+
+
 int main(int argc, char* argv[])
 {
 
   boost_test_dist bt;
   bt.print_comparison();
+
+// WJB: now some RNG testing -- eventually split out into its own main/.cpp file
+#ifdef HAVE_BOOST
+  using namespace boost;
+  PCout.setf(std::ios::fixed);
+
+  // Define a RNG and initialize it with a reproducible seed.
+  // (The seed is unsigned, otherwise the wrong overload may be selected
+  // when using mt19937 as the base_generator_type.)
+  mt19937 generator(42u);
+
+  PCout << "10 samples of a uniform distribution in [0..1):\n";
+
+  // Define a uniform random number distribution which produces "double"
+  // values between 0 and 1 (0 inclusive, 1 exclusive).
+  uniform_real<> uni_dist(0,1);
+  variate_generator<mt19937&, uniform_real<> > uni(generator, uni_dist);
+
+  // You can now retrieve random numbers from that distribution by means
+  // of a STL Generator interface, i.e. calling the generator like a C-function
+  // with no arguments.
+  for(int i = 0; i < 10; ++i)
+    PCout << uni() << '\n';
+
+  generator.seed(static_cast<unsigned int>(std::time(0)));
+
+  PCout << "\nexperiment: roll a die 10 times:\n";
+
+  // You can save a generator's state by copy construction/assignment.
+  mt19937 saved_generator = generator;
+
+  // When calling other functions which take a generator or distribution
+  // as a parameter, make sure to always call by reference (or pointer).
+  // Calling by value invokes the copy constructor... (yadda-yadda -- UNDERSTOOD/BAD IDEA)
+  experiment(generator);
+
+  PCout << "redo the experiment to verify it:\n";
+  experiment(saved_generator);
+
+  // After that, both generators are equivalent
+  //assert(generator == saved_generator);
+
+  // as a degenerate case, you can set min = max for uniform_int
+  uniform_int<> degen_dist(4,4);
+  variate_generator<mt19937&, uniform_int<> > deg(generator, degen_dist);
+  PCout << deg() << " " << deg() << " " << deg() << std::endl;
+#endif // HAVE_BOOST
 
   return 0;
 }
