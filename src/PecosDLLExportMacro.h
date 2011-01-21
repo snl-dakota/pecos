091e922d472ac8f75010da546e5189dd6b8573f8
zack.galbreath <zack.galbreath@ccfab4c5-ad45-0410-a689-f63b6aebdeca>
2011-01-21 20:20:51 +0000

    BUG: another shot at repairing DLLExportMacro for linux / autotools
    git-svn-id: svn+ssh://software.sandia.gov/svn/pecos/trunk@451 ccfab4c5-ad45-0410-a689-f63b6aebdeca

--- src/PecosDLLExportMacro.h
+++ src/PecosDLLExportMacro.h
@@ -1,10 +1,10 @@
 /* #undef PECOS_BUILD_SHARED_LIBS */
 #if defined (_WIN32) && defined (PECOS_BUILD_SHARED_LIBS)
 #  if defined(PECOS_LIB_EXPORTS_MODE)
-#    define PECOS_LIB_DLL_EXPORT __declspec(dllexport)
+#    define PECOS_EXPORT __declspec(dllexport)
 #  else
-#    define PECOS_LIB_DLL_EXPORT __declspec(dllimport)
+#    define PECOS_EXPORT __declspec(dllimport)
 #  endif
 #else
-#  define PECOS_LIB_DLL_EXPORT
+#  define PECOS_EXPORT
 #endif
