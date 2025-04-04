94eda66062da0cc76b9613850f14f3786b78b166
Michael Eldred <mseldre@sandia.gov>
2025-04-03 20:15:52 -0600

    Pecos: minor updates to enum values; squash a few return warnings

--- src/ActiveKey.hpp
+++ src/ActiveKey.hpp
@@ -1167,8 +1167,7 @@ inline bool ActiveKey::decrement_key(short seq_type, size_t seq_index)
   default:
     PCerr << "Error: ActiveKey::decrement_key() unsupported for sequence type "
 	  << seq_type << std::endl;
-    abort_handler(-1);
-    break;
+    abort_handler(-1);  return false;  break;
   }
 }
 
